114ffa4cf2a2e4a9cfc857fbb51ded39765d6e1e
raj <rkumarranjh0@gmail.com>
2025-05-04 13:16:30 +0530

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -85,7 +85,7 @@ const Home = () => {
         )}
       </main>
 
-      <footer className="bg-gray-800 text-white py-6 mt-[100px]">
+      <footer className="bg-gray-800 text-white py-6 mt-[150px]">
         <div className="max-w-6xl mx-auto px-4 text-center">
           <p>© {new Date().getFullYear()} TechBlog. All rights reserved.</p>
         </div>
